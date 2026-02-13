077c97580d969eaa1c4ca2b3ffd235a7f9286a76
arran4 <111667+arran4@users.noreply.github.com>
2026-02-13 12:42:06 +0000

    chore: update generated code

--- cmd/gosubc/list.go
+++ cmd/gosubc/list.go
@@ -53,7 +53,7 @@ func (c *List) Execute(args []string) error {
 		if arg == "--" {
 			break
 		}
-		if strings.HasPrefix(arg, "-") {
+		if strings.HasPrefix(arg, "-") && arg != "-" {
 			name := arg
 			value := ""
 			hasValue := false
